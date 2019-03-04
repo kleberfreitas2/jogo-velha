88c39b5d322f9408a3581a188c6b7e5c0a7d7bba
kleber ataide de freitas <kuatps@gmail.com>
2019-03-04 16:23:18 -0300

    Commiti inicial

---
+++ JogoVelha/JogoVelha/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace JogoVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPlayer1 = new System.Windows.Forms.Label();
+            this.lblPlayer2 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPlayer1
+            // 
+            this.lblPlayer1.AutoSize = true;
+            this.lblPlayer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer1.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblPlayer1.Location = new System.Drawing.Point(0, 0);
+            this.lblPlayer1.Name = "lblPlayer1";
+            this.lblPlayer1.Size = new System.Drawing.Size(143, 31);
+            this.lblPlayer1.TabIndex = 0;
+            this.lblPlayer1.Text = "PLAYER 1";
+            // 
+            // lblPlayer2
+            // 
+            this.lblPlayer2.AutoSize = true;
+            this.lblPlayer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer2.ForeColor = System.Drawing.Color.ForestGreen;
+            this.lblPlayer2.Location = new System.Drawing.Point(149, 0);
+            this.lblPlayer2.Name = "lblPlayer2";
+            this.lblPlayer2.Size = new System.Drawing.Size(143, 31);
+            this.lblPlayer2.TabIndex = 1;
+            this.lblPlayer2.Text = "PLAYER 2";
+            this.lblPlayer2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(10, 69);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 73);
+            this.btn1.TabIndex = 2;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(108, 69);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 73);
+            this.btn2.TabIndex = 3;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(204, 69);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 73);
+            this.btn3.TabIndex = 4;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(204, 159);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 73);
+            this.btn6.TabIndex = 7;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(108, 159);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 73);
+            this.btn5.TabIndex = 6;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(10, 159);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 73);
+            this.btn4.TabIndex = 5;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(204, 248);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(75, 73);
+            this.btn9.TabIndex = 10;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(108, 248);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(75, 73);
+            this.btn8.TabIndex = 9;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(10, 248);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(75, 73);
+            this.btn7.TabIndex = 8;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 365);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lblPlayer2);
+            this.Controls.Add(this.lblPlayer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPlayer1;
+        private System.Windows.Forms.Label lblPlayer2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+    }
+}
+
---
+++ JogoVelha/JogoVelha/Form1.cs
@@ -0,0 +1,529 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoVelha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblPlayer1.Enabled = true;
+            lblPlayer2.Enabled = false;
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+
+            if (lblPlayer1.Enabled == true)
+                btn1.Text = "X";
+            else
+                btn1.Text = "O";
+
+            btn1.Enabled = false;
+
+            if (verificarjogada(1))
+            {
+                if (lblPlayer1.Enabled == true)
+                    MessageBox.Show("Player 1 Venceu!");
+                else
+                    MessageBox.Show("Player 2 Venceu!");
+
+                limparJogo();
+
+            }
+            else
+            {
+                if (lblPlayer1.Enabled == true)
+                {
+                    lblPlayer1.Enabled = false;
+                    lblPlayer2.Enabled = true;
+                }
+                else
+                {
+                    lblPlayer1.Enabled = true;
+                    lblPlayer2.Enabled = false;
+                }
+
+            }
+
+            lblPlayer1.Focus();
+        }
+
+        private bool verificarjogada(int casa)
+        {
+            bool retorno = false;
+
+            if (verificahorizontal(casa))
+                retorno = true;
+            else if (verificaVertical(casa))
+                retorno = true;
+            else if (verificadiagonal(casa))
+                retorno = true;
+
+            return retorno;
+        }
+
+        private bool verificahorizontal(int casa)
+        {
+            bool retorno = false;
+            int linha = 0;
+
+
+            if (casa == 1 || casa == 2 || casa == 3)
+                linha = 1;
+            else if (casa == 4 || casa == 5 || casa == 6)
+                linha = 2;
+            else
+                linha = 3;
+
+            if (lblPlayer1.Enabled == true)
+            {
+                if (linha == 1 && btn1.Text == "X" && btn2.Text == "X" && btn3.Text == "X")
+                    retorno = true;
+                if (linha == 2 && btn4.Text == "X" && btn5.Text == "X" && btn6.Text == "X")
+                    retorno = true;
+                if (linha == 3 && btn7.Text == "X" && btn8.Text == "X" && btn9.Text == "X")
+                    retorno = true;
+
+            }
+            else
+            {
+                if (linha == 1 && btn1.Text == "O" && btn2.Text == "O" && btn3.Text == "O")
+                    retorno = true;
+                if (linha == 2 && btn4.Text == "O" && btn5.Text == "O" && btn6.Text == "O")
+                    retorno = true;
+                if (linha == 3 && btn7.Text == "O" && btn8.Text == "O" && btn9.Text == "O")
+                    retorno = true;
+
+            }
+
+
+            return retorno;
+        }
+
+        private bool verificaVertical(int casa)
+        {
+            bool retorno = false;
+            int coluna = 0;
+
+
+            if (casa == 1 || casa == 4 || casa == 7)
+                coluna = 1;
+            else if (casa == 2 || casa == 5 || casa == 8)
+                coluna = 2;
+            else
+                coluna = 3;
+
+            if (lblPlayer1.Enabled == true)
+            {
+                if (coluna == 1 && btn1.Text == "X" && btn4.Text == "X" && btn7.Text == "X")
+                    retorno = true;
+                if (coluna == 2 && btn2.Text == "X" && btn5.Text == "X" && btn8.Text == "X")
+                    retorno = true;
+                if (coluna == 3 && btn3.Text == "X" && btn6.Text == "X" && btn9.Text == "X")
+                    retorno = true;
+
+            }
+            else
+            {
+                if (coluna == 1 && btn1.Text == "O" && btn4.Text == "O" && btn7.Text == "O")
+                    retorno = true;
+                if (coluna == 2 && btn2.Text == "O" && btn5.Text == "O" && btn8.Text == "O")
+                    retorno = true;
+                if (coluna == 3 && btn3.Text == "O" && btn6.Text == "O" && btn9.Text == "O")
+                    retorno = true;
+
+            }
+
+
+            return retorno;
+        }
+
+        private bool verificadiagonal(int casa)
+        {
+            bool retorno = false;
+            int diagonal = 0;
+
+            //verifica se casa faz parte diagonal
+
+            if (casa == 2 || casa == 4 || casa == 6 || casa == 8)
+                return false;
+            else
+            {
+                if (casa == 1 || casa == 5 || casa == 9)
+                    diagonal = 1;
+                else
+                    diagonal = 2;
+
+                if (lblPlayer1.Enabled == true)
+                {
+                    if (diagonal == 1 && btn1.Text == "X" && btn5.Text == "X" && btn9.Text == "X")
+                        retorno = true;
+                    if (diagonal == 2 && btn3.Text == "X" && btn5.Text == "X" && btn7.Text == "X")
+                        retorno = true;
+
+
+                }
+                else
+                {
+                    if (diagonal == 1 && btn1.Text == "O" && btn5.Text == "O" && btn9.Text == "O")
+                        retorno = true;
+                    if (diagonal == 2 && btn3.Text == "O" && btn5.Text == "O" && btn7.Text == "O")
+                        retorno = true;
+
+                }
+
+            }
+
+
+
+
+
+
+
+
+            return retorno;
+        }
+        
+        private void limparJogo()
+        {
+            btn1.Text = "";
+            btn2.Text = "";
+            btn3.Text = "";
+            btn4.Text = "";
+            btn5.Text = "";
+            btn6.Text = "";
+            btn7.Text = "";
+            btn8.Text = "";
+            btn9.Text = "";
+
+            lblPlayer1.Enabled = true;
+            lblPlayer2.Enabled = false;
+
+            btn1.Enabled = true;
+            btn2.Enabled = true;
+            btn3.Enabled = true;
+            btn4.Enabled = true;
+            btn5.Enabled = true;
+            btn6.Enabled = true;
+            btn7.Enabled = true;
+            btn8.Enabled = true;
+            btn9.Enabled = true;
+
+
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (lblPlayer1.Enabled == true)
+                btn2.Text = "X";
+            else
+                btn2.Text = "O";
+
+            btn2.Enabled = false;
+
+            if (verificarjogada(2))
+            {
+                if (lblPlayer1.Enabled == true)
+                    MessageBox.Show("Player 1 Venceu!");
+                else
+                    MessageBox.Show("Player 2 Venceu!");
+
+                limparJogo();
+
+            }
+            else
+            {
+                if (lblPlayer1.Enabled == true)
+                {
+                    lblPlayer1.Enabled = false;
+                    lblPlayer2.Enabled = true;
+                }
+                else
+                {
+                    lblPlayer1.Enabled = true;
+                    lblPlayer2.Enabled = false;
+                }
+
+            }
+            lblPlayer1.Focus();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (lblPlayer1.Enabled == true)
+                btn3.Text = "X";
+            else
+                btn3.Text = "O";
+
+            btn3.Enabled = false;
+
+            if (verificarjogada(3))
+            {
+                if (lblPlayer1.Enabled == true)
+                    MessageBox.Show("Player 1 Venceu!");
+                else
+                    MessageBox.Show("Player 2 Venceu!");
+
+                limparJogo();
+
+            }
+            else
+            {
+                if (lblPlayer1.Enabled == true)
+                {
+                    lblPlayer1.Enabled = false;
+                    lblPlayer2.Enabled = true;
+                }
+                else
+                {
+                    lblPlayer1.Enabled = true;
+                    lblPlayer2.Enabled = false;
+                }
+
+            }
+            lblPlayer1.Focus();
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (lblPlayer1.Enabled == true)
+                btn4.Text = "X";
+            else
+                btn4.Text = "O";
+
+            btn4.Enabled = false;
+
+            if (verificarjogada(4))
+            {
+                if (lblPlayer1.Enabled == true)
+                    MessageBox.Show("Player 1 Venceu!");
+                else
+                    MessageBox.Show("Player 2 Venceu!");
+
+                limparJogo();
+
+            }
+            else
+            {
+                if (lblPlayer1.Enabled == true)
+                {
+                    lblPlayer1.Enabled = false;
+                    lblPlayer2.Enabled = true;
+                }
+                else
+                {
+                    lblPlayer1.Enabled = true;
+                    lblPlayer2.Enabled = false;
+                }
+
+            }
+            lblPlayer1.Focus();
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (lblPlayer1.Enabled == true)
+                btn5.Text = "X";
+            else
+                btn5.Text = "O";
+
+            btn5.Enabled = false;
+
+            if (verificarjogada(5))
+            {
+                if (lblPlayer1.Enabled == true)
+                    MessageBox.Show("Player 1 Venceu!");
+                else
+                    MessageBox.Show("Player 2 Venceu!");
+
+                limparJogo();
+
+            }
+            else
+            {
+                if (lblPlayer1.Enabled == true)
+                {
+                    lblPlayer1.Enabled = false;
+                    lblPlayer2.Enabled = true;
+                }
+                else
+                {
+                    lblPlayer1.Enabled = true;
+                    lblPlayer2.Enabled = false;
+                }
+
+            }
+            lblPlayer1.Focus();
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (lblPlayer1.Enabled == true)
+                btn6.Text = "X";
+            else
+                btn6.Text = "O";
+
+            btn6.Enabled = false;
+
+            if (verificarjogada(6))
+            {
+                if (lblPlayer1.Enabled == true)
+                    MessageBox.Show("Player 1 Venceu!");
+                else
+                    MessageBox.Show("Player 2 Venceu!");
+
+                limparJogo();
+
+            }
+            else
+            {
+                if (lblPlayer1.Enabled == true)
+                {
+                    lblPlayer1.Enabled = false;
+                    lblPlayer2.Enabled = true;
+                }
+                else
+                {
+                    lblPlayer1.Enabled = true;
+                    lblPlayer2.Enabled = false;
+                }
+
+            }
+            lblPlayer1.Focus();
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (lblPlayer1.Enabled == true)
+                btn7.Text = "X";
+            else
+                btn7.Text = "O";
+
+            btn7.Enabled = false;
+
+            if (verificarjogada(7))
+            {
+                if (lblPlayer1.Enabled == true)
+                    MessageBox.Show("Player 1 Venceu!");
+                else
+                    MessageBox.Show("Player 2 Venceu!");
+
+                limparJogo();
+
+            }
+            else
+            {
+                if (lblPlayer1.Enabled == true)
+                {
+                    lblPlayer1.Enabled = false;
+                    lblPlayer2.Enabled = true;
+                }
+                else
+                {
+                    lblPlayer1.Enabled = true;
+                    lblPlayer2.Enabled = false;
+                }
+
+            }
+            lblPlayer1.Focus();
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (lblPlayer1.Enabled == true)
+                btn9.Text = "X";
+            else
+                btn9.Text = "O";
+
+            btn9.Enabled = false;
+
+            if (verificarjogada(9))
+            {
+                if (lblPlayer1.Enabled == true)
+                    MessageBox.Show("Player 1 Venceu!");
+                else
+                    MessageBox.Show("Player 2 Venceu!");
+
+                limparJogo();
+
+            }
+            else
+            {
+                if (lblPlayer1.Enabled == true)
+                {
+                    lblPlayer1.Enabled = false;
+                    lblPlayer2.Enabled = true;
+                }
+                else
+                {
+                    lblPlayer1.Enabled = true;
+                    lblPlayer2.Enabled = false;
+                }
+
+            }
+            lblPlayer1.Focus();
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (lblPlayer1.Enabled == true)
+                btn8.Text = "X";
+            else
+                btn8.Text = "O";
+
+            btn8.Enabled = false;
+
+            if (verificarjogada(8))
+            {
+                if (lblPlayer1.Enabled == true)
+                    MessageBox.Show("Player 1 Venceu!");
+                else
+                    MessageBox.Show("Player 2 Venceu!");
+
+                limparJogo();
+
+            }
+            else
+            {
+                if (lblPlayer1.Enabled == true)
+                {
+                    lblPlayer1.Enabled = false;
+                    lblPlayer2.Enabled = true;
+                }
+                else
+                {
+                    lblPlayer1.Enabled = true;
+                    lblPlayer2.Enabled = false;
+                }
+
+            }
+            lblPlayer1.Focus();
+        }
+    }
+}
